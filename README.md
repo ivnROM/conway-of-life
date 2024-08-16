472795589c51d9be6070d7dcc5c8fd11f4a76e00
ivan <meyerivanoliver@gmail.com>
2024-08-16 15:13:53 -0300

    primer setup

---
+++ README.md
@@ -0,0 +1,2 @@
+## RESUMEN
+No hay mucho que comentar, es la implementacion del juego de vida de Conway en rust, proximamente entorno grafico, por ahora, terminal nananashe
---
+++ src/main.rs
@@ -0,0 +1,131 @@
+#![allow(unused_imports, dead_code)]
+
+pub mod game {
+    use std::thread::sleep;
+    use std::time::Duration;
+    use std::fmt::Display;
+
+    pub struct Game {
+        pub cells: [[u32; 6]; 6] 
+    }
+
+    impl Display for Game {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let mut display = String::new();
+            for col in 0..self.cells.len() {
+                for row in 0..self.cells[col].len() {
+                    display.push(char::from_digit(self.cells[col][row], 2).expect("Error con el numero pasado"));
+                }
+                display.push('\n')
+            }
+            write!(f, "{}", display)
+        }
+    }
+
+    impl Game {
+        fn default() -> Self {
+            let arr = [[0; 6]; 6];
+            Game {
+                cells: arr,
+            }
+        }
+
+        pub fn init(&mut self) -> Result<(), &str> {
+            for _ in 0..10 {
+                self.refresh_screen();
+                sleep(Duration::from_secs(1));
+            }
+            Ok(())
+        }
+
+        fn refresh_screen(&mut self) {
+            println!("{self}");
+            for col in 0..self.cells.len() {
+                for row in 0..self.cells[col].len() {
+                    if self.cells[col][row] != 1 {
+                        continue
+                    }
+                    self.cells[col][row] = self.check_neighbors(col, row);
+                }
+            }
+        }
+
+        fn check_neighbors(&mut self, col: usize, row: usize) -> u32 {
+            let mut neighbors = 0;
+            for i in -1..=1 {
+                for j in -1..=1 {
+                    let offset_col = (col as isize + i) as usize;
+                    let offset_row = (row as isize + j) as usize;
+                    if self.cells.get(offset_col).is_none() {
+                        continue
+                        //break
+                    }
+                    if self.cells.get(offset_row).is_none() {
+                        continue
+                    }
+
+                    if self.cells[offset_col][offset_row] == 0 {
+                        self.cells[offset_col][offset_row] = self.dead_check_neighbors(offset_col, offset_row);
+                        continue
+                    }
+                    neighbors += 1;
+                }
+            }
+
+            match neighbors {
+                0 | 1 => {
+                    return 0
+                }
+                4.. => {
+                    return 0
+                }
+                _ => {
+                    return 1
+                }
+            }
+        }
+
+        fn dead_check_neighbors(&mut self, col: usize, row: usize) -> u32 {
+            let mut neighbors = 0;
+            for i in -1..=1 {
+                for j in -1..=1 {
+                    let offset_col = (col as isize + i) as usize;
+                    let offset_row = (row as isize + j) as usize;
+                    if self.cells.get(offset_col).is_none() {
+                        continue
+                        //break
+                    }
+                    if self.cells.get(offset_row).is_none() {
+                        continue
+                    }
+                    if self.cells[offset_col][offset_row] != 1 {
+                        continue
+                    } 
+                    neighbors += 1;
+                }
+            }
+
+            if neighbors != 3 {
+                return 0
+            }
+            1
+        }
+    }
+}
+
+
+fn main() {
+    use game::Game;
+    let mut game = Game {
+        cells: [
+                    [0, 1, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 0]
+               ]
+    };
+    game.init().expect("Fallo en la inicializacion del juego");
+    println!("FIN DEL JUEGO!");
+}
